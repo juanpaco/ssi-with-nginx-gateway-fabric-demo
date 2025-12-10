942366ffd5599ac0d488acbab6b334f319067430
Ethan Garofolo <ethan@suchsoftware.com>
2025-12-10 08:56:36 -0700

    Rebuild script

---
+++ rebuild.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+pushd efferent-web
+./build-and-deploy.sh
+popd
+
+pushd layout-web
+./build-and-deploy.sh
+popd
+
+pushd social-feed-web
+./build-and-deploy.sh
+popd
+
+pushd gateway
+kubectl apply -f /Users/ethan.garfolo/source/ssi/gateway/gateway.yaml
+kubectl apply -f /Users/ethan.garfolo/source/ssi/gateway/http-route.yaml
+kubectl apply -f /Users/ethan.garfolo/source/ssi/gateway/snippets-filter.yaml
+popd
+
+kubectl rollout restart deployment ngf-nginx-gateway-fabric
+kubectl rollout restart deployment web-gw-nginx
+
